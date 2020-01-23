7435b8ea05ee03829c050fe7ad5d1f642f761c0a
hugofeitosa <hugofeitosa93@gmail.com>
2020-01-23 04:13:34 -0300

    alterações no front e remover evento

--- src/main/java/pp2/ifpe/controller/IndexController.java
+++ src/main/java/pp2/ifpe/controller/IndexController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 		
-	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
 	
 	
 	
