d81483c8d3dc18a47e6c68548ae7fef540b15aea
Thuane <thuanealves02@gmail.com>
2020-02-05 02:58:38 -0300

    atualização

--- src/main/java/pp2/ifpe/persistence/IngressoDAO.java
+++ src/main/java/pp2/ifpe/persistence/IngressoDAO.java
@@ -1,11 +1,13 @@
 package pp2.ifpe.persistence;
 
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pp2.ifpe.model.Ingresso;
 
 public interface IngressoDAO extends JpaRepository<Ingresso, Integer>{
 
+	
 	
 }
